d701330ebcdd1c916a338cfc87963e84a7e595a3
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2023-09-17 14:25:01 +0330

    adding a short circuit middleware that check your browser , if it is chrome it will show the 404 status code

---
+++ MiddleWares-Samples/Middlewares/CircuitMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddleWares_Samples.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CircuitMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CircuitMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //Checks the browser , if your browser is chrome , it will not load the web page for u (Short Circuit Middleware)
+
+            await _next(httpContext);
+
+            if (httpContext.Request.Headers["UserAgent"].Any(p => p.ToLower().Contains("Chrome")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CircuitMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCircuitMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CircuitMiddleware>();
+        }
+    }
+}
--- MiddleWares-Samples/Program.cs
+++ MiddleWares-Samples/Program.cs
@@ -22,7 +22,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+//app.UseCircuitMiddleware(); 
 //app.UseContentMiddleware();
 
 app.UseAuthorization();
